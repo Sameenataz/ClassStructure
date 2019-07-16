2a838b367fe172c5d724de705d2020ae1b2024e1
sameena.c.taz <sameena.c.taz@gmail.com>
2019-07-16 22:02:27 +0100

    new changes related to story#120

--- src/learnProgramming/MainClass.java
+++ src/learnProgramming/MainClass.java
@@ -4,6 +4,7 @@ public class MainClass {
     public static void main(String[] args) {
         Person firstObject = new Person();
         Person secondObject = new Person();
+        Person secondObject_story120 = new Person();
 
     }
 }
