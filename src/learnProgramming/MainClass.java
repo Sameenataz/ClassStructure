95e144ad6751d25ec453f0045450a499aa0ad1e2
sameena.c.taz <sameena.c.taz@gmail.com>
2019-07-16 21:50:07 +0100

    this is class structure programe first copy

---
+++ src/learnProgramming/MainClass.java
@@ -0,0 +1,9 @@
+package learnProgramming;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Person firstObject = new Person();
+        Person secondObject = new Person();
+
+    }
+}
---
+++ src/learnProgramming/Person.java
@@ -0,0 +1,32 @@
+package learnProgramming;
+
+public class Person {
+    String firstName;
+    String lastName;
+    int age;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+}
