4551ccb1ff00f4dd393ad1718d7e107b6175d919
sameena.c.taz <sameena.c.taz@gmail.com>
2019-07-16 22:57:06 +0100

    story #122

--- src/learnProgramming/MainClass.java
+++ src/learnProgramming/MainClass.java
@@ -7,7 +7,7 @@ public class MainClass {
         Person secondObject_story120 = new Person();
         Person thirdObject = new Person();
         Person fourthObject = new Person();
-        Person fifthObject = new Person();
+        Person fifthObjectss = new Person();
 
 
     }
